c2331c59332824f0b28e1b4be6985b61254b34d8
Nelson119 <morphlina@gmail.com>
2021-04-19 20:10:30 +0800

    joystick

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Joystick/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Joystick from "@/components/Joystick.vue";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
+    Joystick,
   },
 };
 </script>
