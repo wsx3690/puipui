7df7d718de15d5be2f98a3df3eda3bc732901f28
Nelson119 <morphlina@gmail.com>
2021-04-19 23:58:50 +0800

    joystick MQTT

--- src/components/joyStick.vue
+++ src/components/joyStick.vue
@@ -5,9 +5,16 @@
 
 <script>
 import nipplejs from 'nipplejs';
+import mqtt from 'mqtt';
 
 export default {
   name: 'joystick',
+  data(){
+    return {
+      topic: 'jsd',
+      client: null
+    };
+  },
   mounted () {
     // eslint-disable-next-line no-unused-vars
     const staticGamepad = nipplejs.create({
@@ -19,9 +26,34 @@ export default {
       fadeTime: 400,
       maxNumberOfNipples: 1
     });
-    staticGamepad.on('move', function(evt, data) {
-    console.log(data);
-});
+    this.client  = mqtt.connect('mqtt://test.mosquitto.org:8080');
+
+    this.client.on('connect',  ()=> {
+      this.client.subscribe(this.topic,  (err)=> {
+      console.log(err)
+      })
+    })
+
+    this.client.on('message',  (t, message)=> {
+      // message is Buffer
+     
+      console.log(message.toString())
+    })
+    // staticGamepad.on('move', this.staticGamepadMove);
+    staticGamepad
+      .on('dir:up dir:left dir:down dir:right', this.staticGamepadMove)
+      .on('end', this.staticGamepadMoveEnd)
+  },
+  methods:{
+    staticGamepadMove(evt) {
+      //console.log(evt);
+        this.client.publish(this.topic, evt.type.replace(/dir[:]/,''));
+      //var client  = mqtt.connect('mqtt://test.mosquitto.org')
+
+    },
+    staticGamepadMoveEnd(){
+        this.client.publish(this.topic, 'end');
+    }
   }
 };
 </script>
