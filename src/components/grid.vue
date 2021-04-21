8b52f778af5b07d6c47a01ae8509b05f1b3af463
wsx3690 <wsx369000@gmail.com>
2021-04-21 14:04:54 +0800

    add MQTT in grid

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -7,13 +7,47 @@
       </tr>
       <tr>
         <td>正常/翻車/撞牆</td>
-        <td>°C</td>
+        <td class="temperature_sensor">°C</td>
         <td></td>
       </tr>
     </table>
 </template>
 
+<script>
+import mqtt from 'mqtt';
 
+export default {
+  name: 'temperature_sensor',
+  data(){
+    return {
+      topic: 'temp',    
+      client: null
+    };
+  },
+  mounted () {              //Lifecycle of vue   
+
+    this.client  = mqtt.connect('mqtt://test.mosquitto.org:8080');    //tells the client which broker to connect to.
+
+    this.client.on('connect',  ()=> {
+      this.client.subscribe(this.topic,  (err)=> {    //add a connection event handler that will subscribe the client to a topic. Since our publisher client is publishing messages to the topic, let’s subscribe to the topic so that we can get the messages it sends.
+      console.log(err)
+      })
+    })
+
+    this.client.on('message',  (t, message)=> {     //Let’s also add a message event handler that will log the messages that our subscriber client receives on the topic.
+      // message is Buffer
+      console.log(message.toString())     //print the direction on console
+    })
+    
+    
+    this.client.publish(this.topic, 'I am connected!');
+    setInterval(function() {
+      this.client.publish(this.topic, Math.random().toString()); // published message must be a string
+        }, 5000); // every 5 seconds
+  },
+
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
--- src/components/joyStick.vue
+++ src/components/joyStick.vue
@@ -11,13 +11,13 @@ export default {
   name: 'joystick',
   data(){
     return {
-      topic: 'jsd',
+      topic: 'jsd/web',    
       client: null
     };
   },
-  mounted () {
-    // eslint-disable-next-line no-unused-vars
-    const staticGamepad = nipplejs.create({
+  mounted () {              //Lifecycle of vue   
+    
+    const staticGamepad = nipplejs.create({        //set joystick on web
       zone: document.querySelector('.joystick'),
       mode: 'static',
       position: { left: '50%', top: '50%' },
@@ -26,44 +26,35 @@ export default {
       fadeTime: 400,
       maxNumberOfNipples: 1
     });
-    this.client  = mqtt.connect('mqtt://test.mosquitto.org:8080');
+
+    this.client  = mqtt.connect('mqtt://test.mosquitto.org:8080');    //tells the client which broker to connect to.
 
     this.client.on('connect',  ()=> {
-      this.client.subscribe(this.topic,  (err)=> {
+      this.client.subscribe(this.topic,  (err)=> {    //add a connection event handler that will subscribe the client to a topic. Since our publisher client is publishing messages to the topic, let’s subscribe to the topic so that we can get the messages it sends.
       console.log(err)
       })
     })
 
-    this.client.on('message',  (t, message)=> {
+    this.client.on('message',  (t, message)=> {     //Let’s also add a message event handler that will log the messages that our subscriber client receives on the topic.
       // message is Buffer
      
-      console.log(message.toString())
+      console.log(message.toString())     //print the direction on console
     })
     // staticGamepad.on('move', this.staticGamepadMove);
     staticGamepad
-      .on('dir:up dir:left dir:down dir:right', this.staticGamepadMove)
-      .on('end', this.staticGamepadMoveEnd)
+      .on('dir:up dir:left dir:down dir:right', this.staticGamepadMove)    /* 綁定搖桿事件處理 事件分別為上 下 左 右，用 this.staticGamepadMove publish 值到 broker。第二個參數就是處理的函數(定義在methods 裡面) */
+      .on('end', this.staticGamepadMoveEnd)   /* 綁定搖桿事件處理 放開搖桿(end) 用 this.staticGamepadMoveEnd publish 值到 broker */
   },
   methods:{
     staticGamepadMove(evt) {
       //console.log(evt);
-        this.client.publish(this.topic, evt.type.replace(/dir[:]/,''));
-      //var client  = mqtt.connect('mqtt://test.mosquitto.org')
+        this.client.publish(this.topic, evt.type.replace(/dir[:]/,''));  //publish the same topic to connect to the broker,use replace to remove "dir" 讓他單純pub出up left down right
+      
 
     },
     staticGamepadMoveEnd(){
-        this.client.publish(this.topic, 'end');
+        this.client.publish(this.topic, 'end');  //release joystick,then pub "end"
     }
   }
 };
 </script>
-
-
-
-
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-
-</style>
