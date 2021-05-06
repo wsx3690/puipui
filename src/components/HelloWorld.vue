60ec86387222bc81673d335a120f0c9671471628
Nelson Tan <nelson.tan@hwaotech.com>
2021-05-06 12:32:53 +0800

    復原helloworld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,239 +1,88 @@
 <template>
-  <div class="gauge">
-    <svg :viewBox="`0 0 ${diameter} ${height}`" :style="style" xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <clipPath :id="`clip-bottom-${_uid}`">
-          <rect x="0" y="0" :width="diameter" :height="radius + 10" />
-        </clipPath>
-
-        <clipPath :id="`clip-min-${_uid}`" v-if="minThreshold">
-          <Arc :radius="radius + 2" :thickness="thickness + 4" :offsetY="8" :offsetX="-2" :startAngle="0" :endAngle="minThresholdAngle" />
-        </clipPath>
-
-        <clipPath :id="`clip-max-${_uid}`" v-if="maxThreshold">
-          <Arc :radius="radius + 2" :thickness="thickness + 4" :offsetY="8" :offsetX="-2" :startAngle="maxThresholdAngle" :endAngle="180" />
-        </clipPath>
-      </defs>
-
-      <g :clip-path="`url(#clip-bottom-${_uid})`">
-        <g :style="rotationStyle">
-          <pointer-arcs
-            :radius="radius"
-            :thickness="thickness"
-            :offsetY="10"
-            :activeFill="activeFill"
-            :inactiveFill="inactiveFill"
-            :angle="pointerGap"
-          />
-        </g>
-      </g>
-
-      <g :clip-path="`url(#clip-min-${_uid})`" v-if="minThreshold">
-        <g :style="rotationStyle">
-          <pointer-arcs
-            :radius="radius"
-            :thickness="thickness"
-            :offsetY="10"
-            :activeFill="minThresholdFill"
-            :activeStroke="minThresholdFill"
-            :activeStrokeWidth="1"
-            :inactiveFill="minThresholdFill"
-            :inactiveStroke="minThresholdFill"
-            :inactiveStrokeWidth="1"
-            :angle="pointerGap"
-            :style="minThresholdStyle"
-          />
-        </g>
-      </g>
-
-      <g :clip-path="`url(#clip-max-${_uid})`" v-if="maxThreshold">
-        <g :style="rotationStyle">
-          <pointer-arcs
-            :radius="radius"
-            :thickness="thickness"
-            :offsetY="10"
-            :activeFill="maxThresholdFill"
-            :activeStroke="maxThresholdFill"
-            :activeStrokeWidth="1"
-            :inactiveFill="maxThresholdFill"
-            :inactiveStroke="maxThresholdFill"
-            :inactiveStrokeWidth="1"
-            :angle="pointerGap"
-            :style="maxThresholdStyle"
-          />
-        </g>
-      </g>
-
-      <g :style="rotationStyle">
-        <pointer
-          :radius="radius"
-          :pointerStroke="pointerStroke"
-          :pointerStrokeWidth="pointerStrokeWidth"
-          :pivotRadius="pivotRadius"
-          :pivotFill="pivotFill"
-          :pivotStroke="pivotStroke"
-          :pivotStrokeWidth="pivotStrokeWidth"
-        />
-      </g>
-
-      <labels
-        :min="minLabel ? minLabel : min"
-        :max="maxLabel ? maxLabel : max"
-        :value="displayValue"
-        :dp="dp"
-        :unit="unit"
-        :unitOnArc="unitOnArc"
-        :labelsOnArc="labelsOnArc"
-        :radius="radius"
-        :thickness="thickness"
-        :calculatedFontSize="calculatedFontSize"
-        :offsetY="10 + pivotRadius / 2"
-      />
-    </svg>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br />
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li>
+        <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a>
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a>
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a>
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Gauge',
+  name: 'HelloWorld',
   props: {
-    heading: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    headingLevel: {
-      type: Number,
-      required: false,
-      default: 2,
-    },
-    padding: {
-      type: Boolean,
-      required: false,
-      default: true,
-    },
-    fontSize: {
-      type: String,
-      required: false,
-      default: '1em',
-    },
-    min: {
-      type: Number,
-      required: true,
-    },
-    max: {
-      type: Number,
-      required: true,
-    },
-    minLabel: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    maxLabel: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    value: {
-      type: Number,
-      required: true,
-    },
-    maxThreshold: {
-      type: Number,
-      required: false,
-      default: null,
-    },
-    minThreshold: {
-      type: Number,
-      required: false,
-      default: null,
-    },
-    dp: {
-      type: Number,
-      required: false,
-      default: 0,
-    },
-    unit: {
-      type: String,
-      required: false,
-      default: null,
-    },
-    unitOnArc: {
-      type: Boolean,
-      required: false,
-      default: true,
-    },
-    labelsOnArc: {
-      type: Boolean,
-      required: false,
-      default: true,
-    },
-    valueToExceedLimits: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-    radius: {
-      type: Number,
-      required: false,
-      default: 215,
-    },
-    thickness: {
-      type: Number,
-      required: false,
-      default: 70,
-    },
-    pointerGap: {
-      type: Number,
-      required: false,
-      default: 4,
-    },
-    pointerStroke: {
-      type: String,
-      required: false,
-      default: 'currentcolor',
-    },
-    pointerStrokeWidth: {
-      type: Number,
-      required: false,
-      default: 14,
-    },
-    pivotRadius: {
-      type: Number,
-      required: false,
-      default: 20,
-    },
-    ...styleProps('pivot', {
-      strokeWidth: 2,
-      stroke: 'currentcolor',
-      fill: 'currentcolor',
-    }),
-    minThresholdFill: {
-      type: String,
-      required: false,
-      default: 'none',
-    },
-    maxThresholdFill: {
-      type: String,
-      required: false,
-      default: 'none',
-    },
-    activeFill: {
-      type: String,
-      required: false,
-      default: 'currentcolor',
-    },
-    inactiveFill: {
-      type: String,
-      required: false,
-      default: 'currentcolor',
-    },
-    svgStyle: {
-      type: Object,
-      required: false,
-      default: () => ({}),
-    },
+    msg: String,
   },
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
